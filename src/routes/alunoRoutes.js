754f8ce162ebef88d75af16e1d13baa595908d79
Thiago <southiagorm@gmail.com>
2026-09-11 19:20:18 -0300

    implementação do método findMany, paginação, middlewares, lançamento e tratamento de exceções personalizadas

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -3,6 +3,10 @@ const alunoController = require("../controllers/AlunoController");
 
 const router = express.Router();
 
+router.get("/",(request, response, next)=>{
+    console.log("Executando antes do findMany");
+    next();
+}, alunoController.findMany);
 router.post("/", alunoController.create);
 
 module.exports = router;
